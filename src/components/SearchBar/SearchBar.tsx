15e5da576f1092c57b92f3c95e8f0789bfbf1ada
BeLieVer031 <sandyrajak031@gmail.com>
2023-04-11 23:37:53 +0530

    search bar completed

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,16 +1,53 @@
 import React from 'react'
 import SearchHistory from "../../localdata/SearchHistory.json"
+import HistoryIcon from '@mui/icons-material/History';
+import SearchIcon from '@mui/icons-material/Search';
+
 function SearchBar() {
     const [isFocus, setIsFocus] = React.useState<boolean>(false)
+    const [search, setSearch] = React.useState<string>("")
+    const wrapperRef = React.useRef<HTMLDivElement>(null);
 
+    // @Function for checking out side click of parent_div
+    function useOutsideAlerter(ref: React.MutableRefObject<HTMLDivElement | null>) {
+        React.useEffect(() => {
+            /**
+             * Alert if clicked on outside of element
+             */
+            function handleClickOutside(event: MouseEvent) {
+                if (ref.current && !ref.current.contains(event.target as HTMLInputElement)) {
+                    setIsFocus(false)
+                }
+            }
+            // Bind the event listener
+            document.addEventListener("mousedown", handleClickOutside);
+            return () => {
+                // Unbind the event listener on clean up
+                document.removeEventListener("mousedown", handleClickOutside);
+            };
+        }, [ref]);
+    }
+
+    useOutsideAlerter(wrapperRef)
     return (
-        <div className={`w-1/2 border-2 border-red-600 ${isFocus ? "rounded-2xl" : "rounded-full"} p-2`}>
-            <input type="text" className='w-full text-xl' placeholder='Search in Drive' onFocus={() => setTimeout(() => setIsFocus(true), 200)} onBlur={() => setIsFocus(false)} />
+        <div className={`w-1/2 ${isFocus ? "border-2 rounded-2xl bg-white" : "rounded-full bg-blue-50"}`} ref={wrapperRef}>
+            <div className='flex items-center my-2'>
+                <SearchIcon className='mx-2' />
+                <input type="text" className={`w-3/5 text-xl ${isFocus ? "bg-white" : "bg-blue-50"} outline-none`} placeholder='Search in Drive' onFocus={() => setTimeout(() => setIsFocus(true), 200)} value={search} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearch(e.target.value)} />
+            </div>
             {
-                isFocus ? <div>
+                isFocus ? <div className='pt-2'>
                     {
                         SearchHistory.map((item, index) => {
-                            return <li>{item.history}</li>
+                            return <div className={`p-2 border w-full`}>
+                                <HistoryIcon className='mr-1' />
+                                <span className='cursor-pointer' onClick={() => {
+                                    setIsFocus(false)
+                                    setSearch(item.history)
+                                }}>
+                                    {item.history}
+                                </span>
+                            </div>
                         })
 
                     }
