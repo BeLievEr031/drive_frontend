72f34a05a2c4400482ac3e000732d1a40f63a986
BeLieVer031 <sandyrajak031@gmail.com>
2023-04-17 16:42:25 +0530

    side bar completed

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -3,9 +3,10 @@ import Navbar from '../../components/Navbar/Navbar'
 import Layout from '../../components/Layout/Layout'
 
 function Home() {
+    
     return (
         <Layout>
-            <h1>hello</h1>
+            <h1 className='bg-red-400'>hello</h1>
         </Layout>
     )
 }
